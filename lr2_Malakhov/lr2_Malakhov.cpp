620f8f51544a18141f1f7fda67ea9eb6b2f1c7a4
malakhovkk <50257860+malakhovkk@users.noreply.github.com>
2020-12-16 14:03:21 +0300

    LR3

--- lr2_Malakhov/lr2_Malakhov.cpp
+++ lr2_Malakhov/lr2_Malakhov.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 //#include <regex>
 //#include <string>
 #include "Pipe.h"
@@ -11,6 +12,8 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+
+#define INF 1E6
 
 struct pairCS
 {
@@ -606,6 +609,74 @@ int FindShortestPath(unordered_map<int, vector<pairCS>>& graph, unordered_map<in
     return d[idCS2];
 }
 
+
+//struct edge {
+//    int a, b, cap, flow;
+//};
+//
+//int n, s, t, d[MAXN], ptr[MAXN], q[MAXN];
+//vector<edge> e;
+//vector<int> g[MAXN];
+//
+//void add_edge(int a, int b, int cap) {
+//    edge e1 = { a, b, cap, 0 };
+//    edge e2 = { b, a, 0, 0 };
+//    g[a].push_back(e.size());
+//    e.push_back(e1);
+//    g[b].push_back(e.size());
+//    e.push_back(e2);
+//}
+//
+//bool bfs() {
+//    int qh = 0, qt = 0;
+//    q[qt++] = s;
+//    d(n, -1);
+//    d[s] = 0;
+//    while (qh < qt && d[t] == -1) {
+//        int v = q[qh++];
+//        for (size_t i = 0; i < g[v].size(); ++i) {
+//            int id = g[v][i],
+//                to = e[id].b;
+//            if (d[to] == -1 && e[id].flow < e[id].cap) {
+//                q[qt++] = to;
+//                d[to] = d[v] + 1;
+//            }
+//        }
+//    }
+//    return d[t] != -1;
+//}
+//
+//int dfs(int v, int flow) {
+//    if (!flow)  return 0;
+//    if (v == t)  return flow;
+//    for (; ptr[v] < (int)g[v].size(); ++ptr[v]) {
+//        int id = g[v][ptr[v]],
+//            to = e[id].b;
+//        if (d[to] != d[v] + 1)  continue;
+//        int pushed = dfs(to, min(flow, e[id].cap - e[id].flow));
+//        if (pushed) {
+//            e[id].flow += pushed;
+//            e[id ^ 1].flow -= pushed;
+//            return pushed;
+//        }
+//    }
+//    return 0;
+//}
+//
+//int dinic() {
+//    int flow = 0;
+//    for (;;) {
+//        if (!bfs())  break;
+//        memset(ptr, 0, n * sizeof ptr[0]);
+//        vector<int> ptr()
+//        while (int pushed = dfs(s, INF))
+//            flow += pushed;
+//    }
+//    return flow;
+//}
+
+
+
 int main()
 {
    
@@ -640,11 +711,12 @@ int main()
             "19. Удалить граф" << endl <<
             "20. Удалить ребро из графа" << endl <<
             "21. Поиск кратчайшего пути" << endl <<
+            "21. Поиск максимального потока " << endl <<
             "0. Выход" << endl;
         command = inputNotNegativeInteger("Введите номер команды: ");
-        while (command > 21)
+        while (command > 22)
         {
-            cout << "Введенное число больше 21! ";
+            cout << "Введенное число больше 22! ";
             command = inputNotNegativeInteger("Введите номер команды: ");
         }
 
@@ -939,7 +1011,7 @@ int main()
         }
         case 22:
         {
-
+            break;
         }
         case 0:
         {
